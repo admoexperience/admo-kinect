0d6b0088efd07bffd6776b19158529e218b430fc
David Rubin <david@fireid.com>
2013-09-14 18:19:44 +0200

    Clean up debugging output

--- classes/lib/tasks/CheckInTask.cs
+++ classes/lib/tasks/CheckInTask.cs
@@ -1,19 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NLog;
-
-namespace Admo.classes.lib.tasks
+﻿namespace Admo.classes.lib.tasks
 {
     public class CheckInTask : BaseTask
     {
-        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
-        
         public override void Perform()
         {
-            Logger.Debug("Timer checkin");
             Config.CheckIn();
         }
     }
