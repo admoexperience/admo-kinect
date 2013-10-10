862186cea5e75fdda62e81cb0ed2c44038bb88f9
philliproso <philliproso@gmail.com>
2013-10-10 16:07:48 +0200

    neaten things

--- AdmoTests/GestureDetectionTests.cs
+++ AdmoTests/GestureDetectionTests.cs
@@ -11,18 +11,18 @@ namespace AdmoTests
         {
             var handhead = new HandHead
                 {
-                    HandX = (float) -0.22,
-                    HandY = -(float) -0.05,
-                    HeadY = (float) 0.1
+                    HandX = -0.22f,
+                    HandY =  -0.05f,
+                    HeadY =  0.1f
                 };
             var gestureDetector = new GestureDetection();
-            string myString = "";
+            var myString = "";
 
-            for (int i = 1; i <= 21; i++)
+            for (var i = 0; i < 20; i++)
             {
-                handhead.HandX = handhead.HandX + (float) 0.03;
-                handhead.HandY = handhead.HandY - (float) 0.005;
-                handhead.HeadY = handhead.HeadY + (float) 0.001;
+                handhead.HandX = handhead.HandX +  0.03f;
+                handhead.HandY = handhead.HandY -  0.005f;
+                handhead.HeadY = handhead.HeadY + 0.001f;
                 myString = myString + gestureDetector.DetectSwipe(handhead);
             }
 
