abce793f7a0a14711377d5ba8f3b10867c58bbde
philliproso <philliproso@gmail.com>
2013-10-10 16:22:39 +0200

    Need to place pauses and timers to work when not in debug mode

--- AdmoTests/GestureDetectionTests.cs
+++ AdmoTests/GestureDetectionTests.cs
@@ -17,12 +17,15 @@ namespace AdmoTests
                 };
             var gestureDetector = new GestureDetection();
             var myString = "";
+            myString = myString + gestureDetector.DetectSwipe(handhead);
+            System.Threading.Thread.Sleep(400);
 
             for (var i = 0; i < 20; i++)
             {
                 handhead.HandX = handhead.HandX +  0.03f;
                 handhead.HandY = handhead.HandY -  0.005f;
                 handhead.HeadY = handhead.HeadY + 0.001f;
+                System.Threading.Thread.Sleep(40);
                 myString = myString + gestureDetector.DetectSwipe(handhead);
             }
 
