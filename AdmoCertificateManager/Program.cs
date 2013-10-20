91f9d92b2737c3508c739b6b96a6d401d3fffc32
David Rubin <david@fireid.com>
2013-10-20 14:36:06 +0200

    Clean up of CertificateManager removing statics and making code generally DRY

--- AdmoCertificateManager/Program.cs
+++ AdmoCertificateManager/Program.cs
@@ -3,46 +3,43 @@ using System.Threading;
 
 namespace AdmoCertificateManager
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            var portNumber = CertificateHandler.DefaultPort;
 
-
-
+            //Usage is AdmoCertificateManger.exe $PORT_NUMBER
             if (args.Length > 0)
             {
-                CertificateHandler.Port = args[0];
+                portNumber = args[0];
             }
 
-                var myCert = CertificateHandler.AdmoCert;
-                if (!myCert.HasPrivateKey)
-                    Console.WriteLine("Thats a bit of a bother");
+            var certHandler = new CertificateHandler(portNumber);
 
-                CertificateHandler.AddCertToStore();
-                var authloc = CertificateHandler.CheckIfInStore(CertificateHandler.CertStorAuth, myCert);
-                var localloc = CertificateHandler.CheckIfInStore(CertificateHandler.CertStorLocal, myCert);
+            var myCert = certHandler.AdmoCert;
+            if (!myCert.HasPrivateKey)
+                Console.WriteLine("Certificate does not have a private key");
 
-               
+            certHandler.AddCertToStore();
+            var authloc = certHandler.CheckIfInStore(certHandler.CertStoreAuth, myCert);
+            var localloc = certHandler.CheckIfInStore(certHandler.CertStoerLocal, myCert);
 
-                if (authloc && localloc)
-                {
-                    Console.WriteLine("Certificate loaded");
-                    Thread.Sleep(1000);
-                    Console.WriteLine(CertificateHandler.DeleteOldCerts());
-                    Console.WriteLine(CertificateHandler.BindApp2Cert());
-                    Console.WriteLine(CertificateHandler.GrantPermissionToUseUrl());
-                    Thread.Sleep(2000);
-                }
-                else
-                {
-                    Console.WriteLine("Failed To load certificate " + authloc.ToString()+ localloc.ToString());
-                    Thread.Sleep(1000);
-                }
-            
 
+            if (authloc && localloc)
+            {
+                Console.WriteLine("Certificate loaded");
+                Thread.Sleep(1000);
+                Console.WriteLine(certHandler.DeleteOldCerts());
+                Console.WriteLine(certHandler.BindApp2Cert());
+                Console.WriteLine(certHandler.GrantPermissionToUseUrl());
+                Thread.Sleep(2000);
+            }
+            else
+            {
+                Console.WriteLine("Failed To load certificate " + authloc.ToString() + localloc.ToString());
+                Thread.Sleep(1000);
+            }
         }
-
-       
     }
 }
