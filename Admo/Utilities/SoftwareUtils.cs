8cff384a385f85dc193f09982c8b4eff6e0ada44
David Rubin <david@fireid.com>
2013-11-11 15:17:52 +0200

    Handle system wide installs of chrome

--- Admo/Utilities/SoftwareUtils.cs
+++ Admo/Utilities/SoftwareUtils.cs
@@ -9,9 +9,17 @@ namespace Admo.Utilities
 
         public static string GetChromeVersion()
         {
-            var path = Registry.GetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\App Paths\chrome.exe", "", null);
-            if (path != null)
-                return FileVersionInfo.GetVersionInfo(path.ToString()).FileVersion;
+            //Handle both system wide and user installs of chrome
+            var pathLocal = Registry.GetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\App Paths\chrome.exe", "", null);
+            var pathMachine = Registry.GetValue(@"HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\App Paths\chrome.exe", "", null);
+            if (pathLocal != null)
+            {
+                return FileVersionInfo.GetVersionInfo(pathLocal.ToString()).FileVersion;   
+            }
+            if (pathMachine != null)
+            {
+                return FileVersionInfo.GetVersionInfo(pathMachine.ToString()).FileVersion;   
+            }
             return String.Empty;
         }
     }
