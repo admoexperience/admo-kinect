35eb8833ac901e0f228ebec71cf5bbebbcf616be
philliproso <philliproso@gmail.com>
2013-11-18 11:17:53 +0200

    Made download from web not local

--- AdmoInstallerCustomAction/DownloadRuntime.cs
+++ AdmoInstallerCustomAction/DownloadRuntime.cs
@@ -27,8 +27,8 @@ namespace AdmoInstallerCustomAction
                 var client = new WebClient();
                 client.DownloadProgressChanged += new DownloadProgressChangedEventHandler(client_DownloadProgressChanged);
                 client.DownloadFileCompleted += new AsyncCompletedEventHandler(client_DownloadFileCompleted);
-                //     client.DownloadFileAsync(new Uri("http://admo-downloads.s3-website-eu-west-1.amazonaws.com/unit-installers/KinectRuntime-v1.8-Setup.exe"),
-                client.DownloadFileAsync(new Uri("http://silverserver/public/Admo/Installers/KinectRuntime-v1.8-Setup.exe"),
+                    client.DownloadFileAsync(new Uri("http://admo-downloads.s3-website-eu-west-1.amazonaws.com/unit-installers/KinectRuntime-v1.8-Setup.exe"),
+                //client.DownloadFileAsync(new Uri("http://silverserver/public/Admo/Installers/KinectRuntime-v1.8-Setup.exe"),
                     @"KinectRuntime-v1.8-Setup.exe");
             });
         bgThead.Start();
