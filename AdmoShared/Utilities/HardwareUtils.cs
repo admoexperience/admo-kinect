7b4917e38531edcfa617a88bc68a7c8834d64b72
philliproso <philliproso@gmail.com>
2013-12-02 12:29:02 +0200

    Moved first set of utilities to new AdmoShared project

--- AdmoShared/Utilities/HardwareUtils.cs
+++ AdmoShared/Utilities/HardwareUtils.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Management;
 
-namespace AdmoInstallerCustomAction
+namespace Admo.Utilities
 {
     public class HardwareUtils
     {
---
+++ AdmoShared/Utilities/Utils.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Admo.Utilities
+{
+    public class Utils
+    {
+        public static double GetCurrentTimeInSeconds()
+        {
+            return Convert.ToDouble(DateTime.Now.Ticks) / 10000 / 1000;
+        }
+
+        public static long AsEpochTime(DateTime dateTime)
+        {
+            var span = (dateTime
+                        - new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc));
+            return (long) span.TotalSeconds;
+        }
+
+        public static string ConvertToJson(Object obj)
+        {
+            return JsonConvert.SerializeObject(obj,
+                                       Formatting.None,
+                                       new JsonSerializerSettings
+                                       {
+                                           NullValueHandling = NullValueHandling.Ignore,
+                                           Formatting = Formatting.None,
+                                           ContractResolver = new CamelCasePropertyNamesContractResolver()
+                                       });
+        }
+
+        public static Dictionary<string, object> ParseJson(string result)
+        {
+            return JsonConvert.DeserializeObject<Dictionary<string, object>>(result);
+        }
+
+        public static string Sha256(string fileName)
+        {
+            using (var stream = File.OpenRead(fileName))
+            {
+                var sha = new SHA256Managed();
+                var hash = sha.ComputeHash(stream);
+
+                var shaHash = BitConverter.ToString(hash).Replace("-", string.Empty);
+                return shaHash.ToLowerInvariant();
+            }
+        }
+
+        public static string BytesToHuman(long byteCount)
+        {
+            var suf = new [] {"B", "KB", "MB", "GB", "TB", "PB", "EB"}; //Longs run out around EB
+            if (byteCount == 0)
+                return "0" + suf[0];
+            var bytes = Math.Abs(byteCount);
+            var place = Convert.ToInt32(Math.Floor(Math.Log(bytes, 1024)));
+            var num = Math.Round(bytes / Math.Pow(1024, place), 1);
+            return (Math.Sign(byteCount) * num) + suf[place];
+        }
+
+        public static bool CheckifAngleCanChange(double angleChangeTime, double getCurrentTimeInSeconds)
+        {
+            return ((getCurrentTimeInSeconds - angleChangeTime) > 1);
+        }
+    }
+}
